638bfc43a9326b744b8e1406e96936871512544a
Pedro Fernando de Mattos Baptista <pedrofmattosb@gmail.com>
2026-01-19 16:09:54 -0500

    First Server commit

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+// import page components, not App.vue
+import Home from '../views/Home.vue'
+import Host from '../views/Host.vue'
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/host', component: Host },
+]
+
 const router = createRouter({
   history: createWebHistory(),
-  routes: [
-    { path: '/', component: () => import('../App.vue') },
-    //{ path: '/about', component: () => import('../views/About.vue') },
-  ],
+  routes,
 })
 
 export default router
